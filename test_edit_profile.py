f60c62449933506ccb95eba7a657ce06c512ee54
Enea Jorgji <jorgji.enea@gmail.com>
2020-06-15 00:47:52 +0200

    added all tests

--- test_edit_profile.py
+++ test_edit_profile.py
@@ -1,10 +1,13 @@
 import unittest
+import names
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
+NEW_NAME = names.get_first_name()
 
-class LoginUser(unittest.TestCase):
+
+class EditProfile(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome()
@@ -16,7 +19,21 @@ class LoginUser(unittest.TestCase):
         self.driver.find_element_by_id('login').send_keys(Keys.ENTER)
         time.sleep(3)
         assert ' Zalogowano' in self.driver.title
+        time.sleep(3)
+
+        self.driver.find_element_by_xpath('//img[@src="./icon_profile.png"]').click()
+        time.sleep(3)
+
+        self.driver.find_element_by_id('editOSobie').click()
+        self.driver.find_element_by_id('editOSobie').send_keys(
+            'For the reason of test I used the module of the name, so I would be able to check whether this section has been changed ',
+            NEW_NAME)
+        time.sleep(3)
+
+        self.driver.find_element_by_id('editBttn').click()
         time.sleep(5)
+
+        assert 'Dane ' in self.driver.page_source
 
     def tearDown(self):
         self.driver.close()
