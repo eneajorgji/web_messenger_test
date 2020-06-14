a7f2e159c0f3e7d28dccc4313e692fb0f22fd108
Enea Jorgji <jorgji.enea@gmail.com>
2020-06-14 22:22:07 +0200

    fixed test_login

--- test_login_correct.py
+++ test_login_correct.py
@@ -1,7 +1,7 @@
 import unittest
 from selenium import webdriver
-import time
 from selenium.webdriver.common.keys import Keys
+import time
 
 
 class LoginUser(unittest.TestCase):
@@ -9,8 +9,7 @@ class LoginUser(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
 
-    def login(self):
-        # driver = self.driver
+    def test_login(self):
         self.driver.get('http://kmg.hcm.pl/testowanie/index.html')
         self.driver.find_element_by_id('userLogin').send_keys('adam442')
         self.driver.find_element_by_id('passwordLogin').send_keys('adam442')
@@ -19,10 +18,9 @@ class LoginUser(unittest.TestCase):
         # assert 'PSTO' in self.driver.page_source
         time.sleep(5)
 
-
     def tearDown(self):
         self.driver.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
--- test_register_new_user.py
+++ test_register_new_user.py
@@ -4,7 +4,7 @@ import time
 import names
 
 NEW_NAME = names.get_first_name()
-
+NEW_SURNAME = names.get_last_name()
 
 class RegisterNewUser(unittest.TestCase):
 
@@ -18,12 +18,12 @@ class RegisterNewUser(unittest.TestCase):
         time.sleep(2)
         self.driver.find_element_by_id('pass1').send_keys('Test123')
         self.driver.find_element_by_id('pass2').send_keys('Test123')
-        self.driver.find_element_by_id('name').send_keys('Name1')
-        self.driver.find_element_by_id('surname').send_keys('Surname1')
+        self.driver.find_element_by_id('name').send_keys(NEW_NAME)
+        self.driver.find_element_by_id('surname').send_keys(NEW_SURNAME)
         self.driver.find_element_by_id('kodgrupy').send_keys('wsb_g2')
         self.driver.find_element_by_id('register').click()
         assert 'Rejestracja ' in self.driver.page_source
-        time.sleep(5)
+        time.sleep(3)
 
     def tearDown(self):
         self.driver.close()
