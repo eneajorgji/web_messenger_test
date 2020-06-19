e88604c979f1846fa55872606eecca00fbd4b794
Enea Jorgji <jorgji.enea@gmail.com>
2020-06-19 17:41:49 +0200

    removed: time.sleep, added: implicity_wait and wait.until

--- test_login_correct.py
+++ test_login_correct.py
@@ -1,22 +1,26 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class LoginUser(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome()
+        self.driver.get('http://kmg.hcm.pl/testowanie/index.html')
+        self.driver.implicitly_wait(10)
 
     def test_login(self):
-        self.driver.get('http://kmg.hcm.pl/testowanie/index.html')
         self.driver.find_element_by_id('userLogin').send_keys('adam442')
         self.driver.find_element_by_id('passwordLogin').send_keys('adam442')
         self.driver.find_element_by_id('login').send_keys(Keys.ENTER)
-        time.sleep(3)
-        assert ' Zalogowano' in self.driver.title
-        time.sleep(5)
+
+        wait = WebDriverWait(self.driver, 10)
+        self.driver.element = wait.until(EC.title_contains('PSTO webMessenger - Zalogowano'))
+
+        assert 'PSTO webMessenger - Zalogowano' in self.driver.title
 
     def tearDown(self):
         self.driver.close()
